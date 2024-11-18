c482232b26c4e370718cb2679cbdf81bdfed0975
Theodosios Pountridis <tpountridis@epu.ntua.gr>
2024-11-18 21:14:36 +0000

    Update api.py

--- deeptsf_backend/api.py
+++ deeptsf_backend/api.py
@@ -104,7 +104,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://deeptsf.dev.deployai.eu/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
