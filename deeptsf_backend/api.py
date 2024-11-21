c22150066bc1913067d91bab759a969e236de66c
Giannis Xydias <gxidias@epu.ntua.gr>
2024-11-21 11:09:29 +0000

    Update api.py

--- deeptsf_backend/api.py
+++ deeptsf_backend/api.py
@@ -104,7 +104,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://deeptsf.dev.deployai.eu"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
