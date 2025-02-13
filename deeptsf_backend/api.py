56bf12dc9f3e16f592e7d96584c84677a81f9bea
Theodosios Pountridis <tpountridis@epu.ntua.gr>
2025-02-13 11:27:53 +0200

    Add virto

--- deeptsf_backend/api.py
+++ deeptsf_backend/api.py
@@ -118,7 +118,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://deeptsf.aiodp.ai", 
+                   "https://deeptsf.stage.aiodp.ai",
+                   "https://deeptsf.dev.aiodp.ai", 
+                   "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
